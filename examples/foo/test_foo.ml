5164b5bd29214faa5f431c5cb0de593a3223ffc3
andrewray <evilkidder@gmail.com>
2016-11-05 16:41:45 +0000

    based on @yallop suggestion; coercions for callbacks

--- examples/foo/test_foo.ml
+++ examples/foo/test_foo.ml
@@ -11,5 +11,13 @@ let () = add_foo_ptr (addr a) (addr b) (addr c)
 let () = print_foo c
 
 let c = add_foo a b 
-let c = map_foo_a c incr
+
+let coerce t fn = coerce (Foreign.funptr t) (static_funptr t) fn
+
+let cb_t = int @-> returning int
+                  
+let c = map_foo_a c (coerce cb_t incr)
+let () = print_foo c
+
+let c = map_foo_a c (coerce cb_t (fun x -> x*2))
 let () = print_foo c
